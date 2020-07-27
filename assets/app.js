566c37ab88cd138a384d11ceeab2bf15c0a49a8f
DelphicGen <gennardokidam@gmail.com>
2020-07-27 18:55:02 +0700

    Carousel Events

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     let upcomingEvents = document.querySelectorAll('.upcoming-event');
+    let details = document.querySelectorAll('.detail');
     let active = document.querySelector('.active');
     let prev = document.querySelector('.prev');
     let next = document.querySelector('.next');
@@ -19,17 +20,22 @@ $(document).ready(function () {
             $(active).addClass('prev').removeClass('active');
             $(prev).addClass('hidden-event').removeClass('prev');
             $(upcomingEvents[(nextIndex + 1) % upcomingEvents.length]).addClass('next').removeClass('hidden-event');
+            $(details[activeIndex]).addClass('hidden-event');
+            $(details[nextIndex]).removeClass('hidden-event');
         } else {
             $(prev).addClass('active').removeClass('prev');
             $(active).addClass('next').removeClass('active');
             $(next).addClass('hidden-event').removeClass('next');
             $(upcomingEvents[(prevIndex + 4) % upcomingEvents.length]).addClass('prev').removeClass('hidden-event');
+            $(details[activeIndex]).addClass('hidden-event');
+            $(details[prevIndex]).removeClass('hidden-event');
         }
 
         active = document.querySelector('.active');
         prev = document.querySelector('.prev');
         next = document.querySelector('.next');
         upcomingEvents = document.querySelectorAll('.upcoming-event');
+        details = document.querySelectorAll('.detail');
         activeIndex = direction === 'left' ? (activeIndex + 1) % upcomingEvents.length : (activeIndex + 4) % upcomingEvents.length;
         nextIndex = (activeIndex + 1) % upcomingEvents.length;
         prevIndex = (activeIndex + 4) % upcomingEvents.length;
