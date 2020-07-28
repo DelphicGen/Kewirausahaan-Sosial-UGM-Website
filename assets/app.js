a83a46810ab9b824d88eb7e6d2217707d4acbdc3
DelphicGen <gennardokidam@gmail.com>
2020-07-28 21:45:40 +0700

    Landing Page v1

--- assets/app.js
+++ assets/app.js
@@ -1,46 +1,68 @@
 $(document).ready(function () {
-    let mentors = document.querySelectorAll('.item');
-    $('.owl-carousel').owlCarousel({
+    let mentors = document.querySelectorAll('.mentor');
+    $('.owl-carousel-1').owlCarousel({
         loop:false,
         margin:10,
         nav:true,
         responsive:{
             0:{
-                items: mentors.length / 2 >= 1 ? 1 : mentors.length / 2
+                items: mentors.length >= 1 ? 1 : mentors.length
             },
             576:{
-                items: mentors.length / 2 >= 2 ? 2 : mentors.length / 2
+                items: mentors.length >= 2 ? 2 : mentors.length
             },
             768:{
-                items: mentors.length / 2 >= 3 ? 3 : mentors.length / 2
+                items: mentors.length >= 3 ? 3 : mentors.length
             },
             992: {
-                items: mentors.length / 2 >= 4 ? 4 : mentors.length / 2
+                items: mentors.length >= 4 ? 4 : mentors.length
             }
         }
     });
 
-    // Our Mentor
-    // let pages = document.querySelectorAll('.page');
-    // let mentors = document.querySelectorAll('.mentor');
-    // let activePageIndex = 0;
-    // let mentorsContainerWidth = $('.mentors-container').width();
-    // let mentorsWidth = ($('.mentor').width() + 12) * mentors.length - 12 ;
+    let members = document.querySelectorAll('.member');
+    $('.owl-carousel-2').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items: members.length >= 1 ? 1 : members.length
+            },
+            576:{
+                items: members.length >= 2 ? 2 : members.length
+            },
+            768:{
+                items: members.length >= 3 ? 3 : members.length
+            },
+            992: {
+                items: members.length >= 4 ? 4 : members.length
+            }
+        }
+    });
 
-    // console.log(pages, mentorsContainerWidth, mentorsWidth, mentorsContainerWidth/mentorsWidth);
-
-    // pages.forEach((page, index) => {
-    //     $(page).click(function() {
-    //         $(pages[activePageIndex]).removeClass('active-page');
-    //         $(page).addClass('active-page');
-    //         activePageIndex = index;
-    //     })
-    // })
+    let testimonials = document.querySelectorAll('.testimonial');
+    $('.owl-carousel-3').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items: testimonials.length >= 1 ? 1 : testimonials.length
+            },
+            576:{
+                items: testimonials.length >= 2 ? 2 : testimonials.length
+            },
+            992: {
+                items: testimonials.length >= 3 ? 3 : testimonials.length
+            }
+        }
+    });
 
     // Upcoming Event
     let upcomingEvents = document.querySelectorAll('.upcoming-event');
     let details = document.querySelectorAll('.detail');
-    let active = document.querySelector('.active');
+    let activeEvent = document.querySelector('.active-event');
     let prev = document.querySelector('.prev');
     let next = document.querySelector('.next');
     let leftControl = document.querySelector('.left-control');
@@ -55,16 +77,6 @@ $(document).ready(function () {
         else slide('left');
     })
 
-    // $('.upcoming-event').on('swipeleft', function(event) {
-    //     slide('left');
-    // }).on('swiperight', function(event) {   
-    //     slide('right');
-    // })
-
-    // $(".upcoming-event").swiperight(function() {
-    //     slide('right');
-    // });
-
     $(leftControl).on('click', function() {
         slide('right');
     });
@@ -75,22 +87,22 @@ $(document).ready(function () {
 
     const slide = (direction) => {
         if(direction === 'left') {
-            $(next).addClass('active').removeClass('next');
-            $(active).addClass('prev').removeClass('active');
+            $(next).addClass('active-event').removeClass('next');
+            $(activeEvent).addClass('prev').removeClass('active-event');
             $(prev).addClass('hidden-event').removeClass('prev');
             $(upcomingEvents[(nextIndex + 1) % upcomingEvents.length]).addClass('next').removeClass('hidden-event');
             $(details[activeIndex]).addClass('hidden-event');
             $(details[nextIndex]).removeClass('hidden-event');
         } else {
-            $(prev).addClass('active').removeClass('prev');
-            $(active).addClass('next').removeClass('active');
+            $(prev).addClass('active-event').removeClass('prev');
+            $(activeEvent).addClass('next').removeClass('active-event');
             $(next).addClass('hidden-event').removeClass('next');
             $(upcomingEvents[(prevIndex + 4) % upcomingEvents.length]).addClass('prev').removeClass('hidden-event');
             $(details[activeIndex]).addClass('hidden-event');
             $(details[prevIndex]).removeClass('hidden-event');
         }
 
-        active = document.querySelector('.active');
+        activeEvent = document.querySelector('.active-event');
         prev = document.querySelector('.prev');
         next = document.querySelector('.next');
         upcomingEvents = document.querySelectorAll('.upcoming-event');
