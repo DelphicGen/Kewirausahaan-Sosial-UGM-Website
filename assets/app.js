e4b3b112bffcb929e3b7f51f3a151fcb8efbd379
DelphicGen <gennardokidam@gmail.com>
2020-07-28 15:39:39 +0700

    Mentor and Team member

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,43 @@
 $(document).ready(function () {
+    let mentors = document.querySelectorAll('.item');
+    $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items: mentors.length / 2 >= 1 ? 1 : mentors.length / 2
+            },
+            576:{
+                items: mentors.length / 2 >= 2 ? 2 : mentors.length / 2
+            },
+            768:{
+                items: mentors.length / 2 >= 3 ? 3 : mentors.length / 2
+            },
+            992: {
+                items: mentors.length / 2 >= 4 ? 4 : mentors.length / 2
+            }
+        }
+    });
+
+    // Our Mentor
+    // let pages = document.querySelectorAll('.page');
+    // let mentors = document.querySelectorAll('.mentor');
+    // let activePageIndex = 0;
+    // let mentorsContainerWidth = $('.mentors-container').width();
+    // let mentorsWidth = ($('.mentor').width() + 12) * mentors.length - 12 ;
+
+    // console.log(pages, mentorsContainerWidth, mentorsWidth, mentorsContainerWidth/mentorsWidth);
+
+    // pages.forEach((page, index) => {
+    //     $(page).click(function() {
+    //         $(pages[activePageIndex]).removeClass('active-page');
+    //         $(page).addClass('active-page');
+    //         activePageIndex = index;
+    //     })
+    // })
+
+    // Upcoming Event
     let upcomingEvents = document.querySelectorAll('.upcoming-event');
     let details = document.querySelectorAll('.detail');
     let active = document.querySelector('.active');
@@ -13,12 +52,18 @@ $(document).ready(function () {
     $('.upcoming-event').on('click', function() {
         if($(this).hasClass('next')) slide('left');
         else if ($(this).hasClass('prev')) slide('right');
-    }).on('swipeleft', function() {
-        console.log('Hi')
-        slide('left');
-    }).on('swiperight', function() {
-        slide('right');
+        else slide('left');
     })
+
+    // $('.upcoming-event').on('swipeleft', function(event) {
+    //     slide('left');
+    // }).on('swiperight', function(event) {   
+    //     slide('right');
+    // })
+
+    // $(".upcoming-event").swiperight(function() {
+    //     slide('right');
+    // });
 
     $(leftControl).on('click', function() {
         slide('right');
@@ -55,7 +100,4 @@ $(document).ready(function () {
         prevIndex = (activeIndex + 4) % upcomingEvents.length;
 
     }
-
-    
-
 });
